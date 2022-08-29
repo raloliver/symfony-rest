a7bdf6604c0d918c061e5c39c8f032d7ffcd1b3b
Israel Oliveira <contato@raloliver.com.br>
2022-08-29 17:45:10 +0100

    feature/specialty (#7)
    * Init specialty (like a category) with maker
    
    * fixed error when add a doctor with specialty
    
    * fixed update doctor
    
    * Init CRUD specialty
    
    * Finish specialty and the base
    
    Co-authored-by: israelloliveira <israel.oliveira@doutorfinancas.pt>

--- src/Helper/DoctorFactory.php
+++ src/Helper/DoctorFactory.php
@@ -3,16 +3,28 @@
 namespace App\Helper;
 
 use App\Entity\Doctor;
+use App\Repository\SpecialtyRepository;
 
 class DoctorFactory
 {
+    private $specialtyRepository;
+
+    public function __construct(SpecialtyRepository $specialtyRepository)
+    {
+        $this->specialtyRepository = $specialtyRepository;
+    }
+
     public function createDoctor(string $json): Doctor
     {
         $JSON = json_decode($json);
+        $specialtyId = $JSON->specialty;
+        $specialty = $this->specialtyRepository->find($specialtyId);
 
         $doctor = new Doctor();
-        $doctor->crm = $JSON->crm;
-        $doctor->fullName = $JSON->fullName;
+        $doctor
+            ->setCrm($JSON->crm)
+            ->setFullName($JSON->fullName)
+            ->setSpecialty($specialty);
 
         return $doctor;
     }
